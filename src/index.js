dad14e53ee374af86c147e3c5cab54bceb19332e
Viet Truong <viettruong0825@gmail.com>
2023-06-01 15:43:19 +0700

    refactor home and search to site and SiteController

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-var handlebars = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+var handlebars = require('express-handlebars');
 
 const app = express();
 const port = 4000;
 
-const route = require("./routes");
+const route = require('./routes');
 
 route(app);
 
 // HTTP
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 // Using static file
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // Using middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Template engine
-app.engine("handlebars", handlebars.engine());
-app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "resources", "views"));
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 // console.log(path.join(__dirname, "views"));
 
 app.listen(port, () => {
