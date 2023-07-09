5e1c6bbd4a508eaca8d628fe0dd9c59b3d06e16c
Viet Truong <viettruong0825@gmail.com>
2023-07-09 21:02:13 +0700

    fix _id type number and fix countDocumentDeleted

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,19 +3,26 @@ const Course = require('../models/Course');
 class MeController {
     // [GET] /stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .lean()
-            .then((courses) => {
-                res.render('me/stored_courses', { courses });
-            })
-            .catch(next);
+        let courseQuery = Course.find({}).lean();
 
-        Course.countDocumentsDeleted()
-            .lean()
-            .then((deletedCounts) => {
-                console.log(deletedCounts);
-            })
-            .catch(next);
+        Promise.all([courseQuery, Course.countDocumentsDeleted()]).then(
+            ([courses, deleted]) => {
+                res.render('me/stored_courses', { courses, deleted });
+            }
+        );
+        // Course.find({})
+        //     .lean()
+        //     .then((courses) => {
+        //         res.render('me/stored_courses', { courses });
+        //     })
+        //     .catch(next);
+
+        // Course.countDocumentsDeleted()
+        //     .lean()
+        //     .then((deletedCounts) => {
+        //         console.log(deletedCounts);
+        //     })
+        //     .catch(next);
     }
 
     // [GET] /trash/courses
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const slug = require('mongoose-slug-updater');
 const mongooseDelete = require('mongoose-delete');
 const Schema = mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 // const ObjectId = Schema.ObjectId;
 
 const Course = new Schema(
     {
+        _id: { type: Number },
         name: { type: String, required: true },
         description: { type: String, required: true },
         image: String,
@@ -13,11 +15,13 @@ const Course = new Schema(
         videoID: { type: String, unique: true },
     },
     {
+        _id: false,
         timestamps: true,
     }
 );
 
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, { deletedAt: true, overrideMethods: 'all' });
 
 module.exports = mongoose.model('Course', Course);
